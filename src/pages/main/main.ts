454f3b00e9e2e253f5a228ef3a02c4fcd4749612
obed lorisson <obed.lorisson@gmail.com>
2017-01-31 15:36:20 -0500

    add ionic resources

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { HomePage } from '../home/home';
 /*
   Generated class for the Main page.
 
@@ -12,6 +12,19 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'main.html'
 })
 export class MainPage {
+
+  tab1Root = HomePage;
+  tab2Root = HomePage;
+  tab3Root = HomePage;
+
+   doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
