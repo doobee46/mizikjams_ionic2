9bcc3a8a60254f77a40c8c133ba2ab81c4d384db
obed lorisson <obed.lorisson@gmail.com>
2017-02-05 03:02:03 -0500

    add playlist creation alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform, AlertController } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 import { IntroPage } from '../pages/intro/intro';
 import { MainPage } from '../pages/main/main';
 import { ProfilePage } from '../pages/profile/profile';
 import { PlaylistPage } from '../pages/playlist/playlist';
+
 
 @Component({
   templateUrl: 'app.html'
@@ -16,7 +17,7 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform) {
+  constructor(public platform: Platform, public alertCtrl: AlertController) {
     this.initializeApp();
 
     this.pages =[
@@ -28,6 +29,34 @@ export class MyApp {
   openPage(page){
     this.nav.setRoot(page.component);
   }
+
+ doPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Create Playlist',
+      message: "Enter a name for this new Playlist",
+      inputs: [
+        {
+          name: name,
+          placeholder: 'name'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
+ }
   
   openMain(){
     this.nav.setRoot(MainPage);
